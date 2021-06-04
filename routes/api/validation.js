67ed6f04ddc578f469224f0f6c34e47e7bf480bd
Anna Sokolova <sokolovaskorik@gmail.com>
2021-06-04 10:34:00 +0200

    Changed validation

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,17 +1,20 @@
 const Joi = require("joi");
 
 const schemaCreateContact = Joi.object({
-  name: Joi.string().alphanum().min(2).max(30).required(),
+  name: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{2,30}$")).required(),
   email: Joi.string().email({ minDomainSegments: 2 }).required(),
-  phone: Joi.string().pattern("/^(+4|7|8)?[s-]?(?[489][0-9]{2})?[s-]?[0-9]{3}[s-]?[0-9]{2}[s-]?[0-9]{2}$/").required(),
-
+  phone: Joi.string()
+    .pattern(/^[(][\d]{3}[)]\s[\d]{3}[\s][\d]{4}/)
+    .required(),
   isFavorite: Joi.boolean().optional(),
 });
 
 const schemaUpdateContact = Joi.object({
-  name: Joi.string().alphanum().min(2).max(30).optional(),
+  name: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{2,30}$")).optional(),
   email: Joi.string().email({ minDomainSegments: 2 }).optional(),
-  phone: Joi.string().pattern("/^(+4|7|8)?[s-]?(?[489][0-9]{2})?[s-]?[0-9]{3}[s-]?[0-9]{2}[s-]?[0-9]{2}$/").optional(),
+  phone: Joi.string()
+    .pattern(/^[(][\d]{3}[)]\s[\d]{3}[\s][\d]{4}/)
+    .optional(),
   isFavorite: Joi.boolean().optional(),
 }).or("name", "email", "phone", "favorite");
 
@@ -26,7 +29,7 @@ const validate = async (schema, obj, next) => {
   } catch (err) {
     next({
       status: 400,
-      message: err.message,
+      message: `missing ${err.message.replace(/"/g, "")} field`,
     });
   }
 };
@@ -35,10 +38,10 @@ module.exports = {
   validationCreateContact: (req, res, next) => {
     return validate(schemaCreateContact, req.body, next);
   },
-  validationUpdateContact: (req, res, next) => {
+  validationUpdateContact: (req, _res, next) => {
     return validate(schemaUpdateContact, req.body, next);
   },
-  validationUpdateStatusContact: (req, res, next) => {
+  validationUpdateStatusContact: (req, _res, next) => {
     return validate(schemaUpdateStatusContact, req.body, next);
   },
 };
