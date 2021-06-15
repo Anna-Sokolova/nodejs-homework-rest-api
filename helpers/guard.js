fcd51372f3877447add284c6cbb76a5d81391b31
Anna Sokolova <sokolovaskorik@gmail.com>
2021-06-15 11:59:02 +0200

    Added guard

---
+++ helpers/guard.js
@@ -0,0 +1,26 @@
+const passport = require("passport");
+require("../config/passport");
+const { HttpCode } = require("./constants");
+require("colors");
+
+const guard = (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    const headerAuth = req.get("Authorization");
+    console.log(headerAuth.blue);
+    let token = null;
+    if (headerAuth) {
+      token = headerAuth.split(" ")[1];
+    }
+    if (err || !user || token !== user?.token) {
+      return res.status(HttpCode.UNAUTHORIZED).json({
+        status: "error",
+        code: HttpCode.UNAUTHORIZED,
+        message: "Email or password is wrong",
+      });
+    }
+    req.user = user; // хешируем юзера
+    return next();
+  })(req, res, next);
+};
+
+module.exports = guard;
