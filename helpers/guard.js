51961c62ac35137598887539407764b93817c64f
Anna Sokolova <sokolovaskorik@gmail.com>
2021-06-17 11:40:05 +0200

    Changed the check condition

--- helpers/guard.js
+++ helpers/guard.js
@@ -3,19 +3,27 @@ require("../config/passport");
 const { HttpCode } = require("./constants");
 require("colors");
 
+// мидлвара Guard создает и проверяет jwt токены
 const guard = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (err, user) => {
     const headerAuth = req.get("Authorization");
-    console.log(headerAuth.blue);
+    console.log(headerAuth.blue); // консолим токен юзера
     let token = null;
     if (headerAuth) {
       token = headerAuth.split(" ")[1];
     }
-    if (err || !user || token !== user?.token) {
+    if (err) {
       return res.status(HttpCode.UNAUTHORIZED).json({
         status: "error",
         code: HttpCode.UNAUTHORIZED,
         message: "Email or password is wrong",
+      });
+    }
+    if (!user || token !== user?.token) {
+      return res.status(HttpCode.UNAUTHORIZED).json({
+        status: "error",
+        code: HttpCode.UNAUTHORIZED,
+        message: "Not authorized",
       });
     }
     req.user = user; // хешируем юзера
